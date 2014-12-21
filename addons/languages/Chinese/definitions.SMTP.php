790158561c637ba6fad0a9b381895be1f48209be
jmchxy <jmchxy@gmail.com>
2014-12-21 18:02:19 +0800

    增加中文简体翻译

---
+++ addons/languages/Chinese/definitions.SMTP.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Chinese Definitions for the SMTP plugin.
+
+$definitions["Server"] = "服务器";
+$definitions["Port"] = "端口";
+$definitions["Authentication"] = "认证";
+$definitions["Normal"] = "无";
+$definitions["TLS"] = "TLS";
+$definitions["SSL"] = "SSL";
